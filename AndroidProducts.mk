de74f89a53a12c5c46ed335e0e11b7263e6068ae
flominatorgd <github.flominatorgd@gmail.com>
2022-12-09 23:30:23 +0100

    trlteduos: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The havocOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_trlteduos.mk
+    $(LOCAL_DIR)/havoc_trlteduos.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_trlteduos-user \
-    lineage_trlteduos-userdebug \
-    lineage_trlteduos-eng
+    havoc_trlteduos-user \
+    havoc_trlteduos-userdebug \
+    havoc_trlteduos-eng
